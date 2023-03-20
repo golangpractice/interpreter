3e9635eb2352b60d8f82802e5fb6ac22d75cf77a
mjayjones13 <mjayjones13@gmail.com>
2023-03-19 21:27:11 -0500

    CH-1: Added documentation to token.go

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -28,16 +28,22 @@ const (
 	LET      = "LET"
 )
 
+// A Token has a TokenType constant and a literal value.
 type Token struct {
 	Type    TokenType
 	Literal string
 }
 
+// keywords stores a map of TokenTypes to help us distinguish between a keyword and an identifier.
 var keywords = map[string]TokenType{
 	"fn":  FUNCTION,
 	"let": LET,
 }
 
+// LookupIdent checks the keywords map to see whether the given identifier is in fact a keyword.
+//
+// If it is, return the keyword's TokenType constant. If not, we return token.IDENT, which is the
+// TokenType for all user-defined identifiers.
 func LookupIdent(ident string) TokenType {
 	if tok, ok := keywords[ident]; ok {
 		return tok
